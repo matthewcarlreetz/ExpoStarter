510e610a44c12b1163bbccb22ff749246e0f1358
Matthew Reetz <matt@headway.io>
2020-09-03 14:47:24 -0500

    Demonstrate context and the useContext hook.

--- src/views/Hooks/UseContext.tsx
+++ src/views/Hooks/UseContext.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { createContext, useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function UseContext() {
+type Counter = { count: number };
+const AppContext = createContext<Counter>({ count: 0 });
+
+const MyRoundedView = () => {
+  const { count } = useContext(AppContext);
   return (
-    <View style={styles.container}>
-      <Text>UseContext</Text>
+    <View style={styles.roundContainer}>
+      <Text>{count}</Text>
     </View>
   );
-}
+};
+
+const UseContext = () => {
+  return (
+    <AppContext.Provider value={{ count: 123 }}>
+      <View style={styles.root}>
+        <MyRoundedView />
+      </View>
+    </AppContext.Provider>
+  );
+};
+
+export default UseContext;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
+  root: {
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     margin: 12,
-    flex: 1,
+  },
+  roundContainer: {
+    margin: 16,
+    borderRadius: 20,
+    padding: 16,
+    backgroundColor: '#ff0000',
   },
 });
